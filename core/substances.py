18ae818dec71533fdfa155d1a8f0dc605d0aed54
medenigklp-crypto <medeni.gklp@gmail.com>
2026-06-28 02:25:53 +0300

    Update substances.py

--- core/substances.py
+++ core/substances.py
@@ -1,1 +1,13 @@
+# core/substances.py
+from dataclasses import dataclass
+
+@dataclass
+class Chemical:
+    formula: str
+    name: str
+    state: str  # 'solid', 'liquid', 'gas'
+    color: str  # HEX Kodu
+    density: float  # g/cm³
+    ph: float
+    temperature: float  # Kelvin
 
